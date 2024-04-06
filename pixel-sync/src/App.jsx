0f84820c36f951592e314dab499a05bcea3d4cd3
priyanshu kumar <dlnpriyanshu@gmail.com>
2024-04-06 17:47:50 +0530

    main page

--- pixel-sync/src/App.jsx
+++ pixel-sync/src/App.jsx
@@ -3,29 +3,21 @@ import { useState } from 'react'
 // import viteLogo from '/vite.svg'
 import './App.css'
 import Sidebar from './components/Sidebar'
-<<<<<<< HEAD
 import Users from './components/Users'
+import Table from './components/Table'
+import Navbar from './components/Navbar'
 function App() {
-
+const [dark,setDark]=useState(0);
   return (
     <div className='container'>
        <Sidebar/>
        <div className='main'>
         
        <Users/>
-       </div>
-=======
-import Navbar from './components/Navbar'
-import Table from './components/Table'
-function App() {
-  const [dark, setDark] = useState(0)
-
-  return (
-    <div className='container'>
-      <Navbar dark={dark} setDark={setDark} />
-      <Sidebar />
       <Table />
->>>>>>> fbbc550f2a04a4ce1469e46c0fa2ffe6c5d16a07
+       </div>
+        <Navbar dark={dark} setDark={setDark} />
+
     </div>
   )
 }
